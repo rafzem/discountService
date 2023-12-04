69ce8ce17d55ff2b9b0f15f490edb0be3fc5687e
rafal.zemla <raf.zem@gmail.com>
2023-12-04 20:51:45 +0100

    Initial commit - project setup

---
+++ src/main/kotlin/com/zemlar/discountService/DiscountServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.zemlar.discountService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscountServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscountServiceApplication>(*args)
+}
